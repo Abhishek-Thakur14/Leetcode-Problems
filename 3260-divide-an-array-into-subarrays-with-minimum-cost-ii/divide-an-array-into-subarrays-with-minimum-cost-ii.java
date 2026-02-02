b9713cd2f0a8001ba47cbee6837804b8f81f70d9
Abhishek Kumar <124916718+Abhishek-Thakur14@users.noreply.github.com>
2026-02-02 12:22:23 +0530

    Time: 101 ms (100.00%) | Memory: 88.6 MB (60.87%) - LeetSync

--- 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.java
+++ 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.java
@@ -1,88 +1,78 @@
-class Multiset {
-  public void add(int num) {
-    map.merge(num, 1, Integer::sum);
-    ++sz;
-  }
-
-  public void remove(int num) {
-    map.merge(num, -1, Integer::sum);
-    if (map.get(num) == 0)
-      map.remove(num);
-    --sz;
-  }
-
-  public int min() {
-    return map.firstEntry().getKey();
-  }
-
-  public int max() {
-    return map.lastEntry().getKey();
-  }
-
-  public int size() {
-    return sz;
-  }
-
-  public boolean contains(int num) {
-    return map.containsKey(num);
-  }
-
-  private TreeMap<Integer, Integer> map = new TreeMap<>();
-  private int sz = 0;
-}
-
 class Solution {
-  public long minimumCost(int[] nums, int k, int dist) {
-    // Equivalently, the problem is to find nums[0] + the minimum sum of the top
-    // k - 1 numbers in nums[i..i + dist], where i > 0 and i + dist < n.
-    long windowSum = 0;
-    Multiset selected = new Multiset();
-    Multiset candidates = new Multiset();
 
-    for (int i = 1; i <= dist + 1; ++i) {
-      windowSum += nums[i];
-      selected.add(nums[i]);
+    PriorityQueue<Integer> small = new PriorityQueue<>((a, b) -> b - a); // max heap
+    PriorityQueue<Integer> large = new PriorityQueue<>();               // min heap
+    Map<Integer, Integer> delayed = new HashMap<>();
+
+    int smallSize = 0;
+    long smallSum = 0;
+
+    public long minimumCost(int[] nums, int k, int dist) {
+        int n = nums.length;
+
+        for (int i = 1; i <= dist + 1; i++) {
+            add(nums[i]);
+        }
+        makeBalance(k);
+
+        long ans = smallSum;
+
+        for (int i = dist + 2; i < n; i++) {
+            remove(nums[i - dist - 1]);
+            add(nums[i]);
+            makeBalance(k);
+            ans = Math.min(ans, smallSum);
+        }
+
+        return ans + nums[0];
     }
 
-    windowSum = balance(windowSum, selected, candidates, k);
-    long minWindowSum = windowSum;
-
-    for (int i = dist + 2; i < nums.length; ++i) {
-      final int outOfScope = nums[i - dist - 1];
-      if (selected.contains(outOfScope)) {
-        windowSum -= outOfScope;
-        selected.remove(outOfScope);
-      } else {
-        candidates.remove(outOfScope);
-      }
-      if (nums[i] < selected.max()) { // nums[i] is a better number.
-        windowSum += nums[i];
-        selected.add(nums[i]);
-      } else {
-        candidates.add(nums[i]);
-      }
-      windowSum = balance(windowSum, selected, candidates, k);
-      minWindowSum = Math.min(minWindowSum, windowSum);
+    private void add(int x) {
+        if (small.isEmpty() || x <= small.peek()) {
+            small.offer(x);
+            smallSum += x;
+            smallSize++;
+        } else {
+            large.offer(x);
+        }
     }
 
-    return nums[0] + minWindowSum;
-  }
+    private void remove(int x) {
+        delayed.put(x, delayed.getOrDefault(x, 0) + 1);
 
-  // Returns the updated `windowSum` by balancing the multiset `selected` to
-  // keep the top k - 1 numbers.
-  private long balance(long windowSum, Multiset selected, Multiset candidates, int k) {
-    while (selected.size() < k - 1) {
-      final int minCandidate = candidates.min();
-      windowSum += minCandidate;
-      selected.add(minCandidate);
-      candidates.remove(minCandidate);
+        if (!small.isEmpty() && x <= small.peek()) {
+            smallSum -= x;
+            smallSize--;
+        }
+        prune(small);
+        prune(large);
     }
-    while (selected.size() > k - 1) {
-      final int maxSelected = selected.max();
-      windowSum -= maxSelected;
-      selected.remove(maxSelected);
-      candidates.add(maxSelected);
+
+    private void makeBalance(int k) {
+        while (smallSize > k - 1) {
+            int x = small.poll();
+            smallSum -= x;
+            smallSize--;
+            large.offer(x);
+            prune(small);
+        }
+
+        while (smallSize < k - 1 && !large.isEmpty()) {
+            int x = large.poll();
+            smallSum += x;
+            smallSize++;
+            small.offer(x);
+            prune(large);
+        }
     }
-    return windowSum;
-  }
+
+    private void prune(PriorityQueue<Integer> pq) {
+        while (!pq.isEmpty()) {
+            int x = pq.peek();
+            if (!delayed.containsKey(x)) break;
+            delayed.put(x, delayed.get(x) - 1);
+            if (delayed.get(x) == 0) delayed.remove(x);
+            pq.poll();
+        }
+    }
 }
