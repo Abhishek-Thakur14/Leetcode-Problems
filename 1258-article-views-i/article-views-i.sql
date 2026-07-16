8d44f910759730817ef0a6f7a82487648726bb9d
Abhishek Kumar <124916718+Abhishek-Thakur14@users.noreply.github.com>
2026-07-16 16:15:47 +0530

    Time: 408 ms (83.93%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
-select DISTINCT author_id as id
-from views
-where author_id=viewer_id
-order by id;
+SELECT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+GROUP BY author_id
+ORDER BY author_id;
